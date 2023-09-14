c3fb0169aa6a6119a1c6fe252f3ec02ef0fd43bf
Phùng Phi Hùng <phihungphung90@gmail.com>
2023-09-14 13:51:36 +0700

    voquangphuc.info

---
+++ thanhdieuft-data/js/music.php
@@ -0,0 +1,78 @@
+<?php
+$____td_unicode____ = JSON_UNESCAPED_UNICODE;
+$____formatspace____ = JSON_UNESCAPED_SLASHES;
+$ThanhDieu_List = [
+    "//files.catbox.moe/l0rlkn.mp3" => "Move Your Body - Thành Nguyên Remix",
+    "//files.catbox.moe/2kxajw.mp3" => "Mong 1 Ngày Anh Nhớ Đến Em",
+    "//thanhdieu.com/files/Dont-Côi-Remake.mp3" => "Don't Côi Remake",
+    "//files.catbox.moe/sly2n7.mp3" => "Cho Em 1 Lần Yêu Remix",
+    "//thanhdieu.com/files/td1.mp3" => "Giày Cao Gót Màu Đỏ Remix",
+    "//files.catbox.moe/lvjzyz.mp3" => "Ai Mang Cô Đơn Đi - Remix Tiktok",
+    "//files.catbox.moe/xk2g35.mp3" => "Sang Xịn Mịn ft. Buông Hàng (Remix)",
+    "//files.catbox.moe/fzj8z3.mp3" => "China Pipa Remix",
+    "//files.catbox.moe/3591d0.mp3" => "Khuất Lối - Remix Tiktok",
+    "//files.catbox.moe/7uoi9v.mp3" => "Yêu Anh Em Nhé - Remix Tiktok",
+    "//thanhdieu.com/files/Về-Bên-Anh.mp3" => "Về Bên Anh - Cucak Remix",
+    "//thanhdieu.com/files/Anh-Đã-Quen-Với-Cô-Đơn.mp3" => "Anh Đã Quen Với Cô Đơn",
+    "//thanhdieu.com/files/Em-Nào-Có-Tội.mp3" => "Em Nào Có Tội Remix",
+    "//files.catbox.moe/s7uknm.mp3" => "Nhất Trên Đời Remix",
+    "//files.catbox.moe/6nfgpa.mp3" => "Anh Thương Em Nhất Mà - Tiktok",
+    "//files.catbox.moe/vtkxdf.mp3" => "Hôm Nay Em Cưới Rồi - Khải Đăng",
+    "//files.catbox.moe/bd8tkx.mp3" => "Nếu Là Anh - Remix Tiktok",
+    "//files.catbox.moe/k25vek.mp3" => "Không Quan Tâm - Remix Tiktok",
+    "//files.catbox.moe/6agbuh.mp3" => "Vietnam I Love - Remix Tiktok",
+    "//files.catbox.moe/ze5y2r.mp3" => "Em Là Kẻ Đáng Thương - Remix Tiktok",
+    "//files.catbox.moe/rls66n.mp3" => "Chạy Về Khóc Với Anh - Remix Tiktok",
+    "//files.catbox.moe/2x6c8t.mp3" => "She Neva Know - Remix Tiktok",
+    "//files.catbox.moe/huah90.mp3" => "Em Mỉm Cười Trong Thật Xinh - Remix Tiktok",
+    "//files.catbox.moe/stmv9n.mp3" => "Hạnh Phúc Đó Em Không Có - NT Mix",
+    "//files.catbox.moe/eryoj7.mp3" => "Âm Thầm Bên Em - QTrung Remix",
+    "//files.catbox.moe/neetzc.mp3" => "Anh Là Của Em - ThahTrung Remix",
+    "//files.catbox.moe/tgtli9.mp3" => "Heroes Tonight X. Magaming - NB Remix",
+    "//files.catbox.moe/ry2pzf.mp3" => "Chuyên Đôi Ta - QTrung Remix",
+    "//files.catbox.moe/388zmi.mp3" => "CTitanium - Harvey X Mit Remix",
+    "//files.catbox.moe/jfkjsu.mp3" => "Hồng Nhan - Kaifo x Ness Remix",
+    "//files.catbox.moe/e18dw8.mp3" => "That Girl - NB Remix",
+    "//files.catbox.moe/0l547d.mp3" => "Nơi Này Có Anh - Hieii Remix",
+    "//files.catbox.moe/kzznkb.mp3" => "Thu Cuối Remix - Tuấn Smile",
+    "//files.catbox.moe/2o8mmp.mp3" => "Về Đây Em Lo - Heii Remix",
+    "//files.catbox.moe/nc6e7g.mp3" => "Bống Bống Bang Bang - Remix",
+    "//files.catbox.moe/2db7ou.mp3" => "Hãy Trao Cho Anh - Remix",
+    "//files.catbox.moe/ly740z.mp3" => "Hạ Còn Vương Nắng - Vinz & EXCLUSIVE",
+    "//files.catbox.moe/921by1.mp3" => "Người Yêu Cũ - KhoiDinhCao Mix",
+    "//files.catbox.moe/5nbwob.mp3" => "Quẻ Bói - Hải DN Ft Viper Remix",
+    "//files.catbox.moe/tt4q32.mp3" => "Gặp Em Đúng Lúc - Ness x VT Remix",
+    "//files.catbox.moe/pw1x1i.mp3" => "Ngôi Nhà Hoa Hồng x Gặp Em Đúng Lúc",
+    "//files.catbox.moe/moibso.mp3" => "Như Một Người Dưng V2 - TiPo Remix",
+    "//files.catbox.moe/1f2rkd.mp3" => "Về Bên Anh ( Vocal Nữ ) - Phong Con Remix",
+    "//files.catbox.moe/o231t9.mp3" => "Là Tự Em Đa Tình - Minh Phùng Remix",
+    "//files.catbox.moe/zd1k4c.mp3" => "BATTE FORTE - THEREON REMIX",
+    "//files.catbox.moe/2dth0h.mp3" => "TUTU - MIT REMIX",
+    "//files.catbox.moe/8p4foa.mp3" => "Nổi Gió Rồi Ver2 - Harvey Remix",
+    "//files.catbox.moe/cpj60i.mp3" => "Như Anh Đã Thấy Em - Ness Remix",
+    "//files.catbox.moe/p5gkgp.mp3" => "BUỒN CỦA ANH REMIX",
+    "//files.catbox.moe/nt2l4r.mp3" => "VỞ KỊCH CỦA EM REMIX - HỒ PHONG AN",
+    "//files.catbox.moe/14g4io.mp3" => "CHẠY NGAY ĐI FT TÚY ÂM - ĐÔNG x THAZH REMIX",
+    "//files.catbox.moe/lfig7z.mp3" => "THAY LÒNG - TIKI REMIX",
+    "//files.catbox.moe/kyd2dn.mp3" => "QUÁ KHỨ KHÔNG THỂ QUÊN REMIX - JALANI MIX",
+    "//files.catbox.moe/k534ef.mp3" => "YÊU KHÁC THƯƠNG HẠI ft KHÔNG BIẾT PHẢI LÀM SAO - NOPER REMIX",
+];
+
+$RandomMusic____ = array_rand($ThanhDieu_List);
+$TotalMusic_____ = count($ThanhDieu_List);
+$Owner_____ = 'ThanhDieu';
+$Today_____ = date('H:i:s d-m-Y');
+$Domain_____ = $_SERVER['HTTP_HOST'];
+$___SourceData___ = [
+    'music' => $RandomMusic____,
+    'name' => $ThanhDieu_List[$RandomMusic____],
+    'value' => $TotalMusic_____,
+    'made' => $Owner_____,
+    'time' => $Today_____,
+    'domain' => $Domain_____,
+];
+
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
+echo json_encode($___SourceData___, $____formatspace____ | $____td_unicode____);
+?>
